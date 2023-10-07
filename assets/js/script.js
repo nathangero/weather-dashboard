9ddda1b8236391724682e6fd66725436d93edca9
nathangero <nathangero@yahoo.com>
2023-10-07 01:15:59 -0700

    implement fetch with basic api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,51 @@
 import { API_KEY } from "./key.js";
 
-const API_CALL = "api.openweathermap.org/"
+const API_CALL = "https://api.openweathermap.org/data/2.5/weather?q="
 
 
 $(function() {
-
+    $("#button-search").on("click", handleSearch);
 });
+
+
+function handleSearch(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    let inputEl = $("#city-name");
+    let cityName = inputEl.val();
+
+    if (cityName) {
+        getWeather(cityName);
+    } else {
+        alert("Please enter a city name");
+    }
+}
+
+function getWeather(cityName) {
+    let requestURL = buildRequestUrl(cityName);
+    console.log(buildRequestUrl(cityName));
+
+    let options = {
+        method: "GET",
+        mode: "cors",
+        credentials: "same-origin",
+    }
+
+    fetch(requestURL, options)
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            alert("Couldn't get city data")
+            console.log("response:", response);
+        }
+    }).then((data) => {
+        let weatherData = data;
+        // console.log(weatherData);
+    })
+}
+
+function buildRequestUrl(cityName) {
+    return API_CALL + cityName +  "&APPID=" + API_KEY;
+}
