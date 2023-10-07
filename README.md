7a4006a1252b86b2c012a8409f335636f86d9a80
nathangero <nathangero@yahoo.com>
2023-10-07 02:47:18 -0700

    implement fetch for current day and forecast

--- README.md
+++ README.md
@@ -12,8 +12,16 @@ Link to deployment is here: [https://nathangero.github.io/weather-dashboard/](ht
 
 [fetch options](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
 
+[async/await](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function)
+
+[remove *all* whitespace](https://stackoverflow.com/a/6623263)
+
+[return multiple values in func](https://stackoverflow.com/a/2917186)
+
 ### Openweather API docs
 
 [Geocoding API](https://openweathermap.org/api/geocoding-api)
 
-[One Call](https://openweathermap.org/api/one-call-3)
+[v2.5 weather docs](https://openweathermap.org/current)
+
+[5-day forecast docs](https://openweathermap.org/forecast5)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 import { API_KEY } from "./key.js";
 
-const API_CALL = "https://api.openweathermap.org/data/2.5/weather?q="
-
+const API_CALL_GEOCODE = "http://api.openweathermap.org/geo/1.0/direct?q="
+const API_CALL_WEATHER = "https://api.openweathermap.org/data/2.5/weather?";
+const API_CALL_FORECAST =  "https://api.openweathermap.org/data/2.5/forecast?";
+const FORECAST_COUNT = 5;
+var units = "imperial"; // Allow user to change?
 
 $(function() {
     $("#button-search").on("click", handleSearch);
@@ -22,9 +25,76 @@ function handleSearch(event) {
     }
 }
 
-function getWeather(cityName) {
-    let requestURL = buildRequestUrl(cityName);
-    // console.log(buildRequestUrl(cityName));
+
+
+/**
+ * Main function getting the weather. Has helper functions to help separate the work and organize code
+ * @param {String} cityName 
+ * @returns 
+ */
+async function getWeather(cityName) {
+    let coordinates = await getCityLatLon(cityName);
+    let lat = coordinates.lat;
+    let lon = coordinates.lon;
+    console.log("lat, lon:", lat, lon)
+
+    if (!lat || !lon) return; // Guard check
+
+    let weatherToday = await fetchWeather(lat, lon);
+    // console.log("weatherToday:", weatherToday);
+
+    if (!weatherToday) return; // Guard check
+
+    let weatherForecast = await fetchForecast(lat, lon);
+    // console.log("weatherForecast:", weatherForecast)
+}
+
+
+
+/**
+ * Gets the latitude and longitude from the city name. Necessary for fetching the resetof Open Weather's data
+ * @param {String} cityName 
+ * @returns 
+ */
+async function getCityLatLon(cityName) {
+    let requestURL = buildGeocodeUrl(cityName);
+    console.log("buildGeocodeUrl:", requestURL)
+    let options = {
+        method: "GET",
+        mode: "cors",
+        cache: "force-cache", // Force cache for faster response
+        credentials: "same-origin",
+    }
+
+    let response = await fetch(requestURL, options)
+    console.log("response:", response)
+
+    let data = await response.json();
+    console.log("data:", data)
+
+    if (!response.ok || data.length <= 0) {
+        alert("Couldn't get city coordinates.\n\nPlease try including either just \"city name\" or \"city name, state (USA only), country code\"")
+        return
+    }
+
+    let lat = data[0].lat;
+    let lon = data[0].lon;
+
+    return {
+        lat: lat,
+        lon: lon
+    }
+}
+
+/**
+ * Gets the current day's weather via latitude and longitude
+ * @param {Number} lat location's latitude
+ * @param {Number} lon location's longitude
+ * @returns JSON of current day's weather
+ */
+async function fetchWeather(lat, lon) {
+    let requestURL = buildWeatherUrl(lat, lon);
+    // console.log("buildWeatherUrl:", requestURL)
 
     let options = {
         method: "GET",
@@ -33,21 +103,73 @@ function getWeather(cityName) {
         credentials: "same-origin",
     }
 
-    fetch(requestURL, options)
-    .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            alert("Couldn't get city data")
-            console.log("response:", response);
-        }
-    }).then((data) => {
-        let weatherData = data;
+    let response = await fetch(requestURL, options)
+    let data = response.json();
+    
+    if (!response.ok || data.length <= 0) {
+        alert("Couldn't get city coordinates.\n\nPlease try including either just \"city name\" or \"city name, state (USA only), country code\"")
+        return
+    }
 
-        console.log(weatherData);
-    })
+    return data;
 }
 
-function buildRequestUrl(cityName) {
-    return API_CALL + cityName +  "&APPID=" + API_KEY;
+/**
+ * Gets the location's 5 day forecast
+ * @param {Number} lat location's latitude
+ * @param {Number} lon location's longitude
+ * @returns JSON of 5 day forecast
+ */
+async function fetchForecast(lat, lon) {
+    let requestURL = buildForecastUrl(lat, lon);
+    // console.log("buildForecastUrl:", requestURL);
+
+    let options = {
+        method: "GET",
+        mode: "cors",
+        cache: "force-cache", // Force cache for faster response
+        credentials: "same-origin",
+    }
+
+    let response = await fetch(requestURL, options)
+    let data = response.json();
+    
+    if (!response.ok || data.length <= 0) {
+        alert("Couldn't get city coordinates.\n\nPlease try including either just \"city name\" or \"city name, state (USA only), country code\"")
+        return
+    }
+
+    return data;
+}
+
+/**
+ * Creates the url for fetch
+ * @param {String} cityName 
+ * @returns String containing the api call
+ */
+function buildGeocodeUrl(cityName) {
+    const QUERY_LIMIT = 1;
+    let cityNameNoSpace = cityName.replace(/\s/g,''); // remove ALL whitespace
+
+    return `${API_CALL_GEOCODE}${cityNameNoSpace}&limit=${QUERY_LIMIT}&appid=${API_KEY}`;
+}
+
+/**
+ * Creates the url for fetch
+ * @param {Number} lat location's latitude
+ * @param {Number} lon location's longitude
+ * @returns String containing the api call
+ */
+function buildWeatherUrl(lat, lon) {
+    return `${API_CALL_WEATHER}lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=${units}`;
+}
+
+/**
+ * Creates the url for fetch
+ * @param {Number} lat location's latitude
+ * @param {Number} lon location's longitude
+ * @returns String containing the api call
+ */
+function buildForecastUrl(lat, lon) {
+    return `${API_CALL_FORECAST}lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=${units}&cnt=${FORECAST_COUNT}`;
 }
